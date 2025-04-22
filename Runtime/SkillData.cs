e2ef8d22b8e0492bdf84a83fa8123e7a7bcebc10
Nikita Dubinchuk <nitoriy@mail.ru>
2025-04-22 14:57:08 -0700

    added basic functuanality charcter and class data

---
+++ Runtime/SkillData.cs
@@ -0,0 +1,33 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace UnityRPGEditor
+{
+    [CreateAssetMenu(menuName = "RPG Editor/New Skill")]
+    public class SkillData : ScriptableObject
+    {
+        [field: SerializeField]
+        public string Name { get; private set; }
+
+        [field: SerializeField, TextArea]
+        public string Descriptions { get; private set; }
+
+        [field: SerializeField, PreviewField(Height = 100)]
+        public Sprite Icon { get; private set; }
+
+        [field: SerializeField]
+        public float Cooldown { get; private set; } = 5f;
+
+        [field: SerializeField]
+        public float CastDuration { get; private set; } = 0.75f;
+
+        [field: SerializeField]
+        public float AfterCastDelay { get; private set; } = 0.25f;
+
+        [field: SerializeField]
+        public int ManaCost { get; private set; } = 5;
+
+    }
+
+}
+
---
+++ Runtime/WeaponData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace UnityRPGEditor
+{
+    [CreateAssetMenu(menuName = "RPG Editor/New Weapon")]
+    public class WeaponData : ItemData
+    {
+        [field: SerializeField]
+        public int Damage { get; private set; } = 5;
+
+        [field: SerializeField]
+        public float AttackSpeed { get; private set; } = 1f;
+
+        [field: SerializeField]
+        public DamageType DamageType { get; private set; } = DamageType.Physical;
+
+        [field: SerializeField]
+        public float Range { get; private set; } = 1.5f;
+
+        [field: SerializeField]
+        public int Durability { get; private set; } = 100;
+    }
+
+    public enum DamageType
+    {
+        Physical,
+        Fire,
+        Cold,
+        Lightning,
+        Necrotic
+    }
+}
